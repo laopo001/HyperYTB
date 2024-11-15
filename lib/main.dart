a45c64783600fb13c6736c10f197c186170739d6
laopo001 <0laopo0@gmail.com>
2024-11-15 10:14:08 +0800

    fix

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,7 @@ import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:app_settings/app_settings.dart';
 import 'package:awesome_notifications/android_foreground_service.dart';
+import 'package:auto_orientation/auto_orientation.dart';
 
 final InAppLocalhostServer localhostServer =
     InAppLocalhostServer(documentRoot: 'assets', port: 18191);
@@ -75,7 +76,8 @@ class NotificationController {
 }
 
 class _MyAppState extends State<MyApp> {
-  final List<Map<String, dynamic>> _services = [];
+  InAppWebViewController? webViewController;
+
   @override
   void initState() {
     debugPrint("kIsWeb: $kIsWeb isDev: $isDev");
@@ -88,7 +90,7 @@ class _MyAppState extends State<MyApp> {
               channelKey: 'basic_channel',
               channelName: 'Basic notifications',
               channelDescription: 'Notification channel for basic tests',
-              defaultColor: Color(0xFF9D50DD),
+              defaultColor: const Color(0xFF9D50DD),
               ledColor: Colors.white),
         ],
         // Channel groups are only visual and are not required
@@ -115,9 +117,7 @@ class _MyAppState extends State<MyApp> {
       }
     });
 
-    WidgetsBinding.instance.addPostFrameCallback((_) {
-      _checkWifiStatus();
-    });
+    WidgetsBinding.instance.addPostFrameCallback((_) {});
 
     super.initState();
   }
@@ -127,188 +127,202 @@ class _MyAppState extends State<MyApp> {
     super.dispose();
   }
 
-  Future<void> _showMyDialog() async {
-    showDialog<void>(
-      context: context,
-      barrierDismissible: false, // 用户必须点击按钮才能关闭对话框
-      builder: (BuildContext context) {
-        return AlertDialog(
-          title: const Text('提示'),
-          content: const SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Text('需要要连接WiFi'),
-                Text('该应用使用局域网连接'),
-              ],
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: const Text('确定'),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-            ),
-            TextButton(
-              child: const Text('打开设置'),
-              onPressed: () {
-                AppSettings.openAppSettingsPanel(AppSettingsPanelType.wifi);
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Future<void> _checkWifiStatus() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
-    if (connectivityResult.contains(ConnectivityResult.wifi)) {
-      debugPrint('连接到 WiFi');
-    } else {
-      debugPrint('未连接到 WiFi');
-      _showMyDialog();
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-          child: Column(children: <Widget>[
-        Expanded(
-          child: InAppWebView(
-            initialSettings: InAppWebViewSettings(
-              mediaPlaybackRequiresUserGesture: false,
-              allowBackgroundAudioPlaying: true,
-              allowsInlineMediaPlayback: true,
-              isInspectable: kDebugMode,
-            ),
-            initialUrlRequest:
-                URLRequest(url: WebUri("https://m.youtube.com/")),
-            onWebViewCreated: (controller) {},
-            onLoadStart: (controller, url) {
-              debugPrint('onLoadStart: $url');
-              controller.addJavaScriptHandler(
-                  handlerName: 'log',
-                  callback: (args) {
-                    // print arguments coming from the JavaScript side!
-                    debugPrint("log $args");
-                    // return data to the JavaScript side
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'getUserDataPath',
-                  callback: (args) async {
-                    var directory = await getApplicationDocumentsDirectory();
-                    return directory.path;
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'readFile',
-                  callback: (args) async {
-                    // print("readFile $args");
-                    String fileName = args[0];
-                    var directory = await getApplicationDocumentsDirectory();
-                    File file = File('${directory.path}/$fileName');
-                    if (await file.exists()) {
-                      return await file.readAsString();
-                    } else {
-                      return '';
-                    }
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'writeFile',
-                  callback: (args) async {
-                    String fileName = args[0];
-                    String content = args[1];
-                    var directory = await getApplicationDocumentsDirectory();
-                    File file = File('${directory.path}/$fileName');
-                    await file.writeAsString(content);
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'readDir',
-                  callback: (args) async {
-                    var directory = await getApplicationDocumentsDirectory();
-                    List<FileSystemEntity> files = directory.listSync();
-                    List<String> fileNames =
-                        files.map((file) => file.path).toList();
-                    return fileNames;
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'createMediaNotification',
-                  callback: (args) async {
-                    if (Platform.isAndroid) {
-                      await AndroidForegroundService
-                          .startAndroidForegroundService(
-                        foregroundStartMode: ForegroundStartMode.stick,
-                        foregroundServiceType:
-                            ForegroundServiceType.mediaPlayback,
-                        content: NotificationContent(
-                            id: notificationId,
-                            actionType: ActionType.Default,
-                            title: 'hyperytb playing!',
-                            channelKey: 'basic_channel',
-                            locked: true,
-                            autoDismissible: false,
-                            notificationLayout: NotificationLayout.MediaPlayer,
-                            category: NotificationCategory.Service),
-                      );
-                    } else {
-                      await AwesomeNotifications().createNotification(
-                          content: NotificationContent(
-                              id: notificationId,
-                              channelKey: 'basic_channel',
-                              actionType: ActionType.Default,
-                              title: 'hyperytb playing!',
-                              locked: true,
-                              autoDismissible: false,
-                              notificationLayout:
-                                  NotificationLayout.MediaPlayer));
-                    }
-                  });
-              controller.addJavaScriptHandler(
-                  handlerName: 'closeMediaNotification',
-                  callback: (args) async {
-                    if (Platform.isAndroid) {
-                      AndroidForegroundService.stopForeground(notificationId);
-                    } else {
-                      AwesomeNotifications().dismiss(notificationId);
-                    }
-                  });
-            },
-            shouldOverrideUrlLoading: (controller, navigationAction) async {
-              var uri = navigationAction.request.url;
-              // var scheme = uri?.scheme;
-              // debugPrint('shouldOverrideUrlLoading: $uri $scheme');
-              if (uri != null && uri.scheme == "mailto") {
-                // debugPrint('Launched test');
-                if (await canLaunchUrl(uri)) {
-                  await launchUrl(uri);
-                }
-                return NavigationActionPolicy.CANCEL;
-              }
-              if (uri != null &&
-                  uri.toString().startsWith("https://www.dadigua.men")) {
-                if (await canLaunchUrl(uri)) {
-                  await launchUrl(uri);
-                }
-                return NavigationActionPolicy.CANCEL;
-              }
-              return NavigationActionPolicy.ALLOW;
-            },
-            onLoadStop: (controller, url) async {
-              debugPrint('onLoadStop: $url');
-
-              // var result = await controller.evaluateJavascript(
-              //     source:
-              //         "setTimeout(() => {window.href='http://localhost:18191/index.html';}, 10000); 123123");
-
-              // debugPrint(result.toString()); // 2
-            },
+    return WillPopScope(
+        onWillPop: () async {
+          if (webViewController != null) {
+            bool canGoBack = await webViewController!.canGoBack();
+            if (canGoBack) {
+              webViewController!.goBack();
+              return false;
+            }
+          }
+          return true;
+        },
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: const Size.fromHeight(0.0), // 设置高度为0
+            child: AppBar(
+                // 其他 AppBar 属性
+                ),
           ),
-        )
-      ])),
-      // floatingActionButton: favoriteButton(),
-    );
+          body: Container(
+              child: Column(children: <Widget>[
+            Expanded(
+              child: InAppWebView(
+                onWebViewCreated: (controller) {
+                  webViewController = controller;
+                },
+                initialSettings: InAppWebViewSettings(
+                  mediaPlaybackRequiresUserGesture: false,
+                  allowBackgroundAudioPlaying: true,
+                  allowsInlineMediaPlayback: true,
+                  // isInspectable: kDebugMode,
+                ),
+                initialUrlRequest:
+                    URLRequest(url: WebUri("https://m.youtube.com/")),
+                onLoadStart: (controller, url) {
+                  debugPrint('onLoadStart: $url');
+
+                  controller.addJavaScriptHandler(
+                      handlerName: 'log',
+                      callback: (args) {
+                        // print arguments coming from the JavaScript side!
+                        debugPrint("log $args");
+                        // return data to the JavaScript side
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'getUserDataPath',
+                      callback: (args) async {
+                        var directory =
+                            await getApplicationDocumentsDirectory();
+                        return directory.path;
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'readFile',
+                      callback: (args) async {
+                        // print("readFile $args");
+                        String fileName = args[0];
+                        var directory =
+                            await getApplicationDocumentsDirectory();
+                        File file = File('${directory.path}/$fileName');
+                        if (await file.exists()) {
+                          return await file.readAsString();
+                        } else {
+                          return '';
+                        }
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'writeFile',
+                      callback: (args) async {
+                        String fileName = args[0];
+                        String content = args[1];
+                        var directory =
+                            await getApplicationDocumentsDirectory();
+                        File file = File('${directory.path}/$fileName');
+                        await file.writeAsString(content);
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'readDir',
+                      callback: (args) async {
+                        var directory =
+                            await getApplicationDocumentsDirectory();
+                        List<FileSystemEntity> files = directory.listSync();
+                        List<String> fileNames =
+                            files.map((file) => file.path).toList();
+                        return fileNames;
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'createMediaNotification',
+                      callback: (args) async {
+                        if (Platform.isAndroid) {
+                          await AndroidForegroundService
+                              .startAndroidForegroundService(
+                            foregroundStartMode: ForegroundStartMode.stick,
+                            foregroundServiceType:
+                                ForegroundServiceType.mediaPlayback,
+                            content: NotificationContent(
+                                id: notificationId,
+                                actionType: ActionType.Default,
+                                title: 'HyperYoutube playing!',
+                                channelKey: 'basic_channel',
+                                locked: true,
+                                autoDismissible: false,
+                                notificationLayout:
+                                    NotificationLayout.MediaPlayer,
+                                category: NotificationCategory.Service),
+                          );
+                        } else {
+                          await AwesomeNotifications().createNotification(
+                              content: NotificationContent(
+                                  id: notificationId,
+                                  channelKey: 'basic_channel',
+                                  actionType: ActionType.Default,
+                                  title: 'HyperYoutube playing!',
+                                  locked: true,
+                                  autoDismissible: false,
+                                  notificationLayout:
+                                      NotificationLayout.MediaPlayer));
+                        }
+                      });
+                  controller.addJavaScriptHandler(
+                      handlerName: 'closeMediaNotification',
+                      callback: (args) async {
+                        if (Platform.isAndroid) {
+                          AndroidForegroundService.stopForeground(
+                              notificationId);
+                        } else {
+                          AwesomeNotifications().dismiss(notificationId);
+                        }
+                      });
+                },
+                shouldOverrideUrlLoading: (controller, navigationAction) async {
+                  var uri = navigationAction.request.url;
+                  var scheme = uri?.scheme;
+                  debugPrint('shouldOverrideUrlLoading: $uri $scheme');
+                  if (uri != null &&
+                      (uri.scheme == "mailto" || uri.scheme == "intent")) {
+                    // debugPrint('Launched test');
+                    if (await canLaunchUrl(uri)) {
+                      await launchUrl(uri);
+                    } else {
+                      debugPrint('Could not launch');
+                    }
+                    return NavigationActionPolicy.CANCEL;
+                  }
+                  return NavigationActionPolicy.ALLOW;
+                },
+                onEnterFullscreen: (controller) {
+                  debugPrint('onEnterFullscreen');
+                  AutoOrientation.landscapeAutoMode();
+                },
+                onExitFullscreen: (controller) {
+                  debugPrint('onExitFullscreen');
+                  AutoOrientation.portraitAutoMode();
+                },
+                onLoadStop: (controller, url) async {
+                  debugPrint('onLoadStop: $url');
+
+                  controller.evaluateJavascript(source: '''
+                    let __f__ = 0;
+                    function addMediaEventListeners() {
+                      const mediaElements = document.querySelectorAll('video, audio');
+                      mediaElements.forEach(media => {
+                        if(media.paused || media.ended){
+                          if(__f__==1){
+                            __f__ = 0;
+                            window.flutter_inappwebview.callHandler('log', 'closeMediaNotification', __f__);
+                            window.flutter_inappwebview.callHandler('closeMediaNotification');
+                          }
+                        } else {
+                          if(__f__==0){
+                            __f__ = 1;
+                            window.flutter_inappwebview.callHandler('log', 'createMediaNotification', __f__);
+                            window.flutter_inappwebview.callHandler('createMediaNotification');
+                          }
+                        }
+                      });
+                    }
+
+                    // 调用函数以添加事件监听器
+                    setInterval(() => {
+                      addMediaEventListeners();
+                    }, 1000);
+                    addMediaEventListeners();
+                  ''');
+
+                  // var result = await controller.evaluateJavascript(
+                  //     source:
+                  //         "setTimeout(() => {window.href='http://localhost:18191/index.html';}, 10000); 123123");
+
+                  // debugPrint(result.toString()); // 2
+                },
+              ),
+            )
+          ])),
+          // floatingActionButton: favoriteButton(),
+        ));
   }
 }
 
